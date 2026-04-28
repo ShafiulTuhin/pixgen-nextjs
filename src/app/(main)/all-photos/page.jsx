8807fa6932a2cbc9dc46b176a2dae58e3adc5579
ShafiulTuhin <tuhin6488@gmail.com>
2026-04-29 03:30:11 +0600

    implemet category filtering

--- src/app/(main)/all-photos/page.jsx
+++ src/app/(main)/all-photos/page.jsx
@@ -4,9 +4,17 @@ import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
-const AllPhotosPage = async () => {
+const AllPhotosPage = async ({ searchParams }) => {
+  const { category } = await searchParams;
+
   const aiData = await getData();
   const categories = await getCategories();
+
+  const filteredPhotos = category
+    ? aiData.filter(
+        (data) => data.category.toLowerCase() == category.toLowerCase(),
+      )
+    : aiData;
   return (
     <div className="px-4 lg:px-0 container mx-auto py-10">
       <h2 className=" font-bold text-3xl bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent">
@@ -14,14 +22,17 @@ const AllPhotosPage = async () => {
       </h2>
       <div className="space-x-4 mt-5">
         {categories.map((cat) => (
-          <Button className="" variant="outline" key={cat.id}>
-            {cat.name}
-          </Button>
+          <Link href={`?category=${cat.name.toLowerCase()}`} key={cat.id}>
+            {" "}
+            <Button className="" variant="outline">
+              {cat.name}
+            </Button>
+          </Link>
         ))}
       </div>
 
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 mt-10 gap-5">
-        {aiData.map((data) => (
+        {filteredPhotos.map((data) => (
           <PhotoCard key={data.id} data={data} />
         ))}
       </div>
